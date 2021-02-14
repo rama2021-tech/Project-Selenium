8a796037142a8d1c8e8d94937cad28bd09e4e8eb
rama2021-tech <vani.rama11@gmail.com>
2021-02-14 16:02:04 +0300

    Completed..

---
+++ src/org/Sample/sample.java
@@ -0,0 +1,17 @@
+package org.Sample;
+
+
+public class sample {
+
+	public static void main(String[] args) {
+		
+		sample s = new sample();
+		s.ug();
+		 	}
+	public void ug() {
+
+    	System.out.println("Undergraduate Course:");
+	}
+   
+	
+}
